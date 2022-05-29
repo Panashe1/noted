5d8775e36a5dc869786755dd753ea78330692e8a
Josh Smith <josh@jshsmth.com>
2022-05-29 12:04:51 +1000

    remove posts table

--- db/schema.rb
+++ db/schema.rb
@@ -43,12 +43,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_003448) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
