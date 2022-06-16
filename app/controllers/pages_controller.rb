26e91decc5b8a74061d75332ede8b3f80174b93b
Josh Smith <josh@jshsmth.com>
2022-06-16 18:08:52 +1000

    chore: start search function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
     @friend = RSpotify::Album.search('Cat')
     @trending = RSpotify::Recommendations.generate(seed_genres: ['rock', 'pop'])
   end
+
+  def show_search_results
+    @search_text = RSpotify::Artist.search(params[:searchText]).first
+  end
 end
