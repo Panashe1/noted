d5cfecf44132d374dace63cc2a921e9835651cf1
Nick Don <nick.don.1102@gmail.com>
2022-07-14 21:28:49 +1000

    album review logic

--- app/controllers/album_reviews_controller.rb
+++ app/controllers/album_reviews_controller.rb
@@ -5,4 +5,21 @@ class AlbumReviewsController < ApplicationController
       @artist = MusicAPI.new.findArtist(artistid)
       @album_review = AlbumReview.new
   end
+
+  def create
+    @album_review = AlbumReview.new(review_params)
+    @album = MusicAPI.new.findAlbum('7mgdTKTCdfnLoa1HXHvLYM')
+    @album_review.api_id = @album.id
+    @album_review.user_id = current_user.id
+    if @album_review.save
+      redirect_to root
+    else
+      render :show
+    end
+  end
+
+  def review_params
+    params.require(:album_review).permit(:description, :rating)
+  end
+
 end
