81b0531cf32e6e125de2e6b72b46c667c21c7df4
ahad abdul <ahad.a2703@gmail.com>
2022-07-19 21:14:36 +1000

    show me button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,12 +10,12 @@ class PagesController < ApplicationController
     # if general search term is provided, search for artist, album, and track
     if params[:searchText]
       @resultsArtist = MusicAPI.new.findArtist(params[:searchText])
-      @top_tracks = @resultsArtist.top_tracks(:US).first(4)
+      @top_tracks = @resultsArtist.top_tracks(:US)
       @top_albums = @resultsArtist.albums(limit: 8, country: 'US')
 
     else
       @resultsArtist = MusicAPI.new.findArtist(params[:id])
-      @top_tracks = @resultsArtist.top_tracks(:US).first(4)
+      @top_tracks = @resultsArtist.top_tracks(:US)
       @top_albums = @resultsArtist.albums(limit: 8, country: 'US')
     end
   end
